5b712f8b5985d9b7345ba17ec482f2dc2fb570a9
rylanhess <rylan@datasembly.com>
2023-03-03 14:26:15 -0700

    structured message pull

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ from nostr.relay_manager import RelayManager
 from nostr.message_type import ClientMessageType
 
 filter1 = Filter(
-                    #authors=["ca0b427245702954452d3876f01081e9b191f4130bbe9530c5278d9f8fd2600f"], 
+                    authors=["ca0b427245702954452d3876f01081e9b191f4130bbe9530c5278d9f8fd2600f"], 
                     kinds=[1]
                 )
 # my_tags =init_filter.add_arbitrary_tag("t",["hashtag","developing"])
@@ -31,6 +31,15 @@ time.sleep(1) # allow the messages to send
 
 while relay_manager.message_pool.has_events():
   event_msg = relay_manager.message_pool.get_event()
-  print(event_msg.event.content)
+  print('--->>>')
+  print('id: '+event_msg.event.id)
+  print('pubkey: '+event_msg.event.public_key)
+  print('created_at: '+str(event_msg.event.created_at))
+  print('kind: '+str(event_msg.event.kind))
+  print('tags: '+ str(event_msg.event.tags))
+  print('content: '+event_msg.event.content)
+  print('sig: '+event_msg.event.signature)
+  print('url: '+event_msg.url)
+  print('<<<---')
   
 relay_manager.close_connections()
